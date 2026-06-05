be9aa746be22e90e81b6aaa0307fd1ea9785b275
Christoph Summerer <info@trlevel.de>
2026-06-05 14:14:04 +0200

    Add atmosphere environment policy data

---
+++ TombEngine/Scripting/Internal/TEN/Flow/Atmosphere/AtmosphereEnvironment.h
@@ -0,0 +1,252 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+#include "Scripting/Include/ScriptInterfaceLevel.h"
+
+namespace TEN::Scripting
+{
+	enum class AtmosphereEnvironmentMode
+	{
+		EarthLike,
+		Indoor,
+		Underground,
+		Underwater,
+		SpaceVacuum,
+		AlienPlanet,
+		ToxicPlanet,
+		ArtificialDome,
+		Fantasy,
+		Custom
+	};
+
+	enum class AtmospherePrecipitationMaterial
+	{
+		Water,
+		Snow,
+		Ash,
+		Sand,
+		Acid,
+		ToxicFluid,
+		Methane,
+		Spores,
+		CrystalDust,
+		RadiationDust,
+		Plasma,
+		MagicEnergy,
+		Custom
+	};
+
+	/// Constants for atmosphere environment modes.
+	// @enum Flow.AtmosphereEnvironmentMode
+	// @pragma nostrip
+	static const std::unordered_map<std::string, AtmosphereEnvironmentMode> ATMOSPHERE_ENVIRONMENT_MODES
+	{
+		/// Earth-like outdoor atmosphere.
+		// @mem EarthLike
+		{ "EarthLike", AtmosphereEnvironmentMode::EarthLike },
+
+		/// Indoor atmosphere.
+		// @mem Indoor
+		{ "Indoor", AtmosphereEnvironmentMode::Indoor },
+
+		/// Underground atmosphere.
+		// @mem Underground
+		{ "Underground", AtmosphereEnvironmentMode::Underground },
+
+		/// Underwater or fully flooded environment.
+		// @mem Underwater
+		{ "Underwater", AtmosphereEnvironmentMode::Underwater },
+
+		/// Space or vacuum environment.
+		// @mem SpaceVacuum
+		{ "SpaceVacuum", AtmosphereEnvironmentMode::SpaceVacuum },
+
+		/// Alien planet with non-standard atmosphere.
+		// @mem AlienPlanet
+		{ "AlienPlanet", AtmosphereEnvironmentMode::AlienPlanet },
+
+		/// Toxic planet with hostile atmosphere defaults.
+		// @mem ToxicPlanet
+		{ "ToxicPlanet", AtmosphereEnvironmentMode::ToxicPlanet },
+
+		/// Artificial dome or controlled habitat.
+		// @mem ArtificialDome
+		{ "ArtificialDome", AtmosphereEnvironmentMode::ArtificialDome },
+
+		/// Fantasy atmosphere where normal physical rules can be bent.
+		// @mem Fantasy
+		{ "Fantasy", AtmosphereEnvironmentMode::Fantasy },
+
+		/// Custom environment mode.
+		// @mem Custom
+		{ "Custom", AtmosphereEnvironmentMode::Custom }
+	};
+
+	/// Constants for atmosphere precipitation materials.
+	// @enum Flow.AtmospherePrecipitationMaterial
+	// @pragma nostrip
+	static const std::unordered_map<std::string, AtmospherePrecipitationMaterial> ATMOSPHERE_PRECIPITATION_MATERIALS
+	{
+		/// Normal water rain.
+		// @mem Water
+		{ "Water", AtmospherePrecipitationMaterial::Water },
+
+		/// Normal snow.
+		// @mem Snow
+		{ "Snow", AtmospherePrecipitationMaterial::Snow },
+
+		/// Falling ash.
+		// @mem Ash
+		{ "Ash", AtmospherePrecipitationMaterial::Ash },
+
+		/// Sand or dust precipitation.
+		// @mem Sand
+		{ "Sand", AtmospherePrecipitationMaterial::Sand },
+
+		/// Acid precipitation.
+		// @mem Acid
+		{ "Acid", AtmospherePrecipitationMaterial::Acid },
+
+		/// Toxic liquid precipitation.
+		// @mem ToxicFluid
+		{ "ToxicFluid", AtmospherePrecipitationMaterial::ToxicFluid },
+
+		/// Methane or similar alien precipitation.
+		// @mem Methane
+		{ "Methane", AtmospherePrecipitationMaterial::Methane },
+
+		/// Organic spores.
+		// @mem Spores
+		{ "Spores", AtmospherePrecipitationMaterial::Spores },
+
+		/// Crystal dust precipitation.
+		// @mem CrystalDust
+		{ "CrystalDust", AtmospherePrecipitationMaterial::CrystalDust },
+
+		/// Radioactive or hazardous dust.
+		// @mem RadiationDust
+		{ "RadiationDust", AtmospherePrecipitationMaterial::RadiationDust },
+
+		/// Plasma precipitation.
+		// @mem Plasma
+		{ "Plasma", AtmospherePrecipitationMaterial::Plasma },
+
+		/// Magic or energy precipitation.
+		// @mem MagicEnergy
+		{ "MagicEnergy", AtmospherePrecipitationMaterial::MagicEnergy },
+
+		/// Custom precipitation material.
+		// @mem Custom
+		{ "Custom", AtmospherePrecipitationMaterial::Custom }
+	};
+
+	struct AtmosphereEnvironmentProfile
+	{
+		bool Enabled{ false };
+		AtmosphereEnvironmentMode Mode{ AtmosphereEnvironmentMode::EarthLike };
+		AtmospherePrecipitationMaterial PrecipitationMaterial{ AtmospherePrecipitationMaterial::Water };
+		bool ArtificialWeather{ false };
+		bool AllowToxicWeather{ false };
+		bool ForceAllowPrecipitation{ false };
+		bool ForceAllowWind{ false };
+		bool VisualOnlyHazards{ true };
+		float DamagePerSecond{ 0.0f };
+
+		bool IsSpaceMode() const;
+		bool IsAlienOrToxicMode() const;
+		bool IsArtificialEnvironment() const;
+		bool AllowsWeatherType(WeatherType type) const;
+		bool AllowsNormalPrecipitation() const;
+		bool AllowsWind() const;
+		bool HasHazardousPrecipitation() const;
+		bool HasGameplayHazard() const;
+	};
+
+	inline bool AtmosphereEnvironmentProfile::IsSpaceMode() const
+	{
+		return Mode == AtmosphereEnvironmentMode::SpaceVacuum;
+	}
+
+	inline bool AtmosphereEnvironmentProfile::IsAlienOrToxicMode() const
+	{
+		return Mode == AtmosphereEnvironmentMode::AlienPlanet || Mode == AtmosphereEnvironmentMode::ToxicPlanet;
+	}
+
+	inline bool AtmosphereEnvironmentProfile::IsArtificialEnvironment() const
+	{
+		return Mode == AtmosphereEnvironmentMode::ArtificialDome || ArtificialWeather;
+	}
+
+	inline bool AtmosphereEnvironmentProfile::AllowsWeatherType(WeatherType type) const
+	{
+		if (!Enabled || type == WeatherType::None)
+			return true;
+
+		if (ForceAllowPrecipitation)
+			return true;
+
+		switch (Mode)
+		{
+		case AtmosphereEnvironmentMode::SpaceVacuum:
+		case AtmosphereEnvironmentMode::Underwater:
+		case AtmosphereEnvironmentMode::Indoor:
+		case AtmosphereEnvironmentMode::Underground:
+			return ArtificialWeather;
+
+		default:
+			return true;
+		}
+	}
+
+	inline bool AtmosphereEnvironmentProfile::AllowsNormalPrecipitation() const
+	{
+		if (!Enabled)
+			return true;
+
+		if (!AllowsWeatherType(WeatherType::Rain))
+			return false;
+
+		switch (PrecipitationMaterial)
+		{
+		case AtmospherePrecipitationMaterial::Water:
+		case AtmospherePrecipitationMaterial::Snow:
+			return true;
+
+		default:
+			return false;
+		}
+	}
+
+	inline bool AtmosphereEnvironmentProfile::AllowsWind() const
+	{
+		if (!Enabled || ForceAllowWind)
+			return true;
+
+		if (Mode == AtmosphereEnvironmentMode::SpaceVacuum)
+			return ArtificialWeather;
+
+		return true;
+	}
+
+	inline bool AtmosphereEnvironmentProfile::HasHazardousPrecipitation() const
+	{
+		switch (PrecipitationMaterial)
+		{
+		case AtmospherePrecipitationMaterial::Acid:
+		case AtmospherePrecipitationMaterial::ToxicFluid:
+		case AtmospherePrecipitationMaterial::RadiationDust:
+		case AtmospherePrecipitationMaterial::Plasma:
+			return true;
+
+		default:
+			return false;
+		}
+	}
+
+	inline bool AtmosphereEnvironmentProfile::HasGameplayHazard() const
+	{
+		return HasHazardousPrecipitation() && !VisualOnlyHazards && DamagePerSecond > 0.0f;
+	}
+}
