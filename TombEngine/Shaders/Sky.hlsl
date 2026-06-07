4f164365b6bfe51e0a49d8ee8303c35164b3145a
Christoph Summerer <info@trlevel.de>
2026-06-07 22:09:45 +0200

    Restore original sky shader

--- TombEngine/Shaders/Sky.hlsl
+++ TombEngine/Shaders/Sky.hlsl
@@ -6,7 +6,6 @@
 #include "./CBSky.hlsli"
 #include "./AnimatedTextures.hlsli"
 #include "./VertexEffects.hlsli"
-#include "./Aurora.hlsli"
 
 struct PixelShaderInput
 {
@@ -32,26 +31,11 @@ PixelShaderInput VS(VertexShaderInput input)
     output.UV = GetUVPossiblyAnimated(input.UV, DecodeIndexInPoly(input.Effects), DecodeAnimationFrameOffset(input.AnimationFrameOffsetIndexHash));
 	output.FogBulbs = ApplyFogBulbs == 1 ? DoFogBulbsForSky(worldPosition) : 0;
 
-	// Temporary aurora prototype marker. Keep this strict so normal sky/horizon layers
-	// are not routed through the aurora shader path.
-	if (abs(Color.w - 2.0f) < 0.01f && ApplyFogBulbs == 0 && Color.x > 0.99f && Color.y > 0.99f && Color.z > 0.99f)
-	{
-		output.Position = float4(input.Position.x / 5120.0f, input.Position.z / 5120.0f, 0.0001f, 1.0f);
-		output.FogBulbs = 0;
-	}
-
 	return output;
 }
 
 float4 PS(PixelShaderInput input) : SV_TARGET
 {
-	// Temporary aurora prototype marker. This must stay stricter than normal sky colors.
-	if (abs(Color.w - 2.0f) < 0.01f && ApplyFogBulbs == 0 && Color.x > 0.99f && Color.y > 0.99f && Color.z > 0.99f)
-	{
-		float3 aurora = DoAuroraFullscreenDome(input.Position.xy, Frame);
-		return float4(aurora * 0.75f, 1.0f);
-	}
-
     if (Animated && Type == 1)
         input.UV = CalculateUVRotate(input.UV, 0);
 	
