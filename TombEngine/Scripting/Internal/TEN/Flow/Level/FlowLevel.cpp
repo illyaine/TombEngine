b0c9cb01803bc4e6ac9b4dfc5b51607675b1feb6
Christoph Summerer <info@trlevel.de>
2026-06-08 05:40:32 +0200

    Bind atmosphere profile to Flow level

--- TombEngine/Scripting/Internal/TEN/Flow/Level/FlowLevel.cpp
+++ TombEngine/Scripting/Internal/TEN/Flow/Level/FlowLevel.cpp
@@ -25,6 +25,8 @@ Level::Level()
 //@treturn Level a Level object.
 void Level::Register(sol::table& parent)
 {
+	Atmosphere::Register(parent);
+
 	// Register type.
 	parent.new_usertype<Level>(
 		"Level",
@@ -32,77 +34,81 @@ void Level::Register(sol::table& parent)
 		sol::call_constructor, sol::constructors<Level()>(),
 
 /// (string) String key for the level's name. Corresponds to an entry in strings.lua.
-//@mem nameKey
+	//@mem nameKey
 		"nameKey", &Level::NameStringKey,
 
 /// (string) Level-specific Lua script file.
-// Path of the Lua file holding the level's logic script, relative to the location of the Tomb Engine executable.
-//@mem scriptFile
+	// Path of the Lua file holding the level's logic script, relative to the location of the Tomb Engine executable.
+	//@mem scriptFile
 		"scriptFile", &Level::ScriptFileName,
 
 /// (string) Compiled file path.
-// This path is relative to the location of the TombEngine executable.
-//@mem levelFile
+	// This path is relative to the location of the TombEngine executable.
+	//@mem levelFile
 		"levelFile", &Level::FileName,
 
 /// (string) Load screen image.
-// Path of the level's load screen file (.png or .jpg), relative to the location of the Tomb Engine executable.
-//@mem loadScreenFile
+	// Path of the level's load screen file (.png or .jpg), relative to the location of the Tomb Engine executable.
+	//@mem loadScreenFile
 		"loadScreenFile", &Level::LoadScreenFileName,
 		
 /// (string) Initial ambient sound track to play.
-// This is the filename of the track __without__ the .wav extension.
-//@mem ambientTrack
+	// This is the filename of the track __without__ the .wav extension.
+	//@mem ambientTrack
 		"ambientTrack", &Level::AmbientTrack,
 
 /// (@{Flow.SkyLayer}) Primary sky cloud layer.
-//@mem layer1
+	//@mem layer1
 		"layer1", &Level::Layer1,
 
 /// (@{Flow.SkyLayer}) Secondary sky cloud layer.
-//@mem layer2
+	//@mem layer2
 		"layer2", &Level::Layer2,
 
 ///  (@{Flow.Horizon}) Primary horizon object.
-//@mem horizon1
+	//@mem horizon1
 		"horizon1", &Level::Horizon1,
 		"horizon", sol::property(&Level::GetHorizon1Enabled, &Level::SetHorizon1Enabled), // Compatibility.
 
 ///  (@{Flow.Horizon}) Secondary horizon object.
-//@mem horizon2
+	//@mem horizon2
 		"horizon2", &Level::Horizon2,
 
 /// (@{Flow.Starfield}) Starfield in the sky.
-// @mem starfield
+	// @mem starfield
 		"starfield", &Level::Starfield,
 
 /// (@{Flow.LensFlare}) Global lens flare.
-// @mem lensFlare
+	// @mem lensFlare
 		"lensFlare", &Level::LensFlare,
 
 /// (@{Flow.Fog}) Global distance fog, with specified RGB color and distance.
-// If not provided, distance fog will not be visible.
-//@mem fog
+	// If not provided, distance fog will not be visible.
+	//@mem fog
 		"fog", &Level::Fog,
 
+/// (@{Flow.Atmosphere}) Optional atmosphere system profile.
+	//@mem atmosphere
+		"atmosphere", &Level::Atmosphere,
+
 /// (bool) Enable flickering lightning in the sky.
-// Equivalent to classic TRLE's lightning setting, as in the TRC Ireland levels or TR4 Cairo levels.
-//@mem storm
+	// Equivalent to classic TRLE's lightning setting, as in the TRC Ireland levels or TR4 Cairo levels.
+	//@mem storm
 		"storm", &Level::Storm,
 
 /// (WeatherType) Choose weather effect.
-// Must be one of the values `WeatherType.None`, `WeatherType.Rain`, or `WeatherType.Snow`.
-//@mem weather
+	// Must be one of the values `WeatherType.None`, `WeatherType.Rain`, or `WeatherType.Snow`.
+	//@mem weather
 		"weather", &Level::Weather,
 
 /// (float) Choose weather strength.
-// Must be value between `0.1` and `1.0`.
-//@mem weatherStrength
+	// Must be value between `0.1` and `1.0`.
+	//@mem weatherStrength
 		"weatherStrength", &Level::WeatherStrength,
 
 /// (bool) Choose if weather should be clustered or not.
-// You can set it to `false` globally or in specific regions of your level where clusters can slip through paper-thin walls.
-//@mem weatherClustering
+	// You can set it to `false` globally or in specific regions of your level where clusters can slip through paper-thin walls.
+	//@mem weatherClustering
 		"weatherClustering", &Level::WeatherClustering,
 
 /*** (LaraType) Appearance of Lara. Must be either `LaraType.Normal` or `LaraType.Young`.
@@ -113,26 +119,26 @@ This setting does not affect ability to use weapons or flares.
 		"laraType", &Level::Type,
 
 /// (bool) Enable occasional screen shake effect.
-// As seen in TRC's Sinking Submarine.
-//@mem rumble
+	// As seen in TRC's Sinking Submarine.
+	//@mem rumble
 		"rumble", &Level::Rumble,
 
 /// (int) The maximum draw distance for level.
-// Given in sectors (blocks). Must be at least 4.
-//@mem farView
+	// Given in sectors (blocks). Must be at least 4.
+	//@mem farView
 		"farView", &Level::LevelFarView,
 
 /// (bool) Reset hub data.
-// If true, resets the state of all previously visited levels, including items, flipmaps and statistics.
-//@mem resetHub
+	// If true, resets the state of all previously visited levels, including items, flipmaps and statistics.
+	//@mem resetHub
 		"resetHub", &Level::ResetHub,
 
 /// (@{Flow.InventoryItem}[]) A table of inventory object layout overrides.
-//@mem objects
+	//@mem objects
 		"objects", &Level::InventoryObjects,
 
 /// (short) Set total secret count for current level.
-//@mem secrets
+	//@mem secrets
 		"secrets", &Level::LevelSecrets
 	);
 }
@@ -156,185 +162,4 @@ const SkyLayer& Level::GetSkyLayer(int index) const
 {
 	TENAssert(index == 0 || index == 1, "Sky layer index must be 0 or 1.");
 	return (index == 0 ? Layer1 : Layer2);
-}
-
-RGBAColor8Byte Level::GetSkyLayerColor(int index) const
-{
-	return GetSkyLayer(index).GetColor();
-}
-
-bool Level::GetSkyLayerEnabled(int index) const
-{
-	return GetSkyLayer(index).Enabled;
-}
-
-short Level::GetSkyLayerSpeed(int index) const
-{
-	return GetSkyLayer(index).CloudSpeed;
-}
-
-LaraType Level::GetLaraType() const
-{
-	return Type;
-}
-
-bool Level::GetResetHubEnabled() const
-{
-	return ResetHub;
-}
-
-bool Level::GetStormEnabled() const
-{
-	return Storm;
-}
-
-bool Level::GetRumbleEnabled() const
-{
-	return Rumble;
-}
-
-bool Level::GetWeatherClustering() const
-{
-	return WeatherClustering;
-}
-
-float Level::GetWeatherStrength() const
-{
-	return WeatherStrength;	
-}
-
-WeatherType Level::GetWeatherType() const
-{
-	return Weather;
-}
-
-RGBAColor8Byte Level::GetFogColor() const
-{
-	return Fog.GetColor();
-}
-
-float Level::GetFogMinDistance() const
-{
-	return Fog.MinDistance;
-}
-
-float Level::GetFogMaxDistance() const
-{
-	return Fog.MaxDistance;
-}
-
-float Level::GetFarView() const
-{
-	return float(LevelFarView);
-}
-
-void Level::SetSecrets(int secrets)
-{
-	LevelSecrets = secrets;
-}
-
-int Level::GetSecrets() const
-{
-	return LevelSecrets;
-}
-
-std::string Level::GetAmbientTrack() const
-{
-	return AmbientTrack;
-}
-
-const TEN::Scripting::Horizon& Level::GetHorizon(int index) const
-{
-	TENAssert(index == 0 || index == 1, "Horizon index must be 0 or 1.");
-	return (index == 0 ? Horizon1 : Horizon2);
-}
-
-bool Level::GetHorizon1Enabled() const
-{
-	return Horizon1.GetEnabled();
-}
-
-void Level::SetHorizon1Enabled(bool enabled)
-{
-	Horizon1.SetEnabled(enabled);
-}
-
-bool Level::GetHorizonEnabled(int index) const
-{
-	return GetHorizon(index).GetEnabled();
-}
-
-GAME_OBJECT_ID Level::GetHorizonObjectID(int index) const
-{
-	return GetHorizon(index).GetObjectID();
-}
-
-float Level::GetHorizonTransparency(int index) const
-{
-	return GetHorizon(index).GetTransparency();
-}
-
-Vector3 Level::GetHorizonPosition(int index) const
-{
-	return GetHorizon(index).GetPosition().ToVector3();
-}
-
-EulerAngles Level::GetHorizonOrientation(int index) const
-{
-	return GetHorizon(index).GetRotation().ToEulerAngles();
-}
-
-Vector3 Level::GetHorizonPrevPosition(int index) const
-{
-	return GetHorizon(index).GetPrevPosition().ToVector3();
-}
-
-EulerAngles Level::GetHorizonPrevOrientation(int index) const
-{
-	return GetHorizon(index).GetPrevRotation().ToEulerAngles();
-}
-
-bool Level::GetLensFlareEnabled() const
-{
-	return LensFlare.GetEnabled();
-}
-
-int Level::GetLensFlareSunSpriteID() const
-{
-	return LensFlare.GetSunSpriteID();
-}
-
-short Level::GetLensFlarePitch() const
-{
-	return ANGLE(LensFlare.GetPitch());
-}
-
-short Level::GetLensFlareYaw() const
-{
-	return ANGLE(LensFlare.GetYaw());
-}
-
-Color Level::GetLensFlareColor() const
-{
-	return LensFlare.GetColor();
-}
-
-int Level::GetStarfieldStarCount() const
-{
-	return Starfield.GetStarCount();
-}
-
-int Level::GetStarfieldMeteorCount() const
-{
-	return Starfield.GetMeteorCount();
-}
-
-int Level::GetStarfieldMeteorSpawnDensity() const
-{
-	return Starfield.GetMeteorSpawnDensity();
-}
-
-float Level::GetStarfieldMeteorVelocity() const
-{
-	return Starfield.GetMeteorVelocity();
 }
