74ccbf4c26f0d60617c43113cb8093bcf609164c
Christoph Summerer <info@trlevel.de>
2026-06-27 23:27:55 +0200

    Add Lighting and HDR settings menu

--- TombEngine/Game/gui.h
+++ TombEngine/Game/gui.h
@@ -76,6 +76,7 @@ namespace TEN::Gui
 		LoadGame,
 		Options,
 		Display,
+		LightingHDR,
 		GeneralActions,
 		VehicleActions,
 		QuickActions,
@@ -85,14 +86,14 @@ namespace TEN::Gui
 
 	struct MenuOption
 	{
-		MenuType	Type = MenuType::None;
+		MenuType Type = MenuType::None;
 		std::string Text = {};
 	};
 
 	struct ObjectList
 	{
-		int			InventoryItem = 0;
-		EulerAngles Orientation	  = EulerAngles::Identity;
+		int InventoryItem = 0;
+		EulerAngles Orientation = EulerAngles::Identity;
 		unsigned short Bright;
 	};
 
@@ -111,22 +112,20 @@ namespace TEN::Gui
 
 		GameConfiguration Configuration = {};
 
-		int	 SelectedScreenResolution = 0;
-		bool IgnoreInput			  = false; // Ignore input until all actions are inactive.
-		int	 NewKeyWaitTimer		  = 0;
+		int SelectedScreenResolution = 0;
+		bool IgnoreInput = false;
+		int NewKeyWaitTimer = 0;
 	};
 
 	class GuiController
 	{
 	private:
-		// Input inquirers
 		bool GuiIsPulsed(ActionID actionID) const;
 		bool GuiIsSelected(bool onClicked = true) const;
 		bool GuiIsDeselected() const;
 		bool CanSelect() const;
 		bool CanDeselect() const;
 
-		// GUI variables
 		Menu MenuToDisplay = Menu::Title;
 		int SelectedOption;
 		int OptionCount;
@@ -135,7 +134,6 @@ namespace TEN::Gui
 		int TimeInMenu = NO_VALUE;
 		SettingsData CurrentSettings;
 
-		// Inventory variables
 		short CombineObject1;
 		short CombineObject2;
 		bool ItemUsed;
@@ -182,7 +180,6 @@ namespace TEN::Gui
 		void CancelInventorySelection();
 		void UseItem(ItemInfo& item, int objectNumber);
 		void UseBinoculars(ItemInfo& item);
-		// Getters
 
 		const InventoryRing& GetRing(RingTypes ringType);
 		int GetSelectedOption();
@@ -194,8 +191,6 @@ namespace TEN::Gui
 		SettingsData& GetCurrentSettings();
 		int GetLoadSaveSelection();
 		int GetLoopedSelectedOption(int selectedOption, int optionCount, bool canLoop);
-
-		// Setters
 
 		void SetSelectedOption(int menu);
 		void SetMenuToDisplay(Menu menu);
