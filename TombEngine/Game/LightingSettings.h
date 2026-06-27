c364cedef1b6c68542966ce1d587b33df7f07706
Christoph Summerer <info@trlevel.de>
2026-06-27 23:43:54 +0200

    Define Lighting and HDR menu options

--- TombEngine/Game/LightingSettings.h
+++ TombEngine/Game/LightingSettings.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include "Game/Gui.h"
+#include "Specific/Input/Input.h"
+#include "Specific/configuration.h"
+
 namespace TEN::Gui
 {
+	enum class LightingSettingsOption
+	{
+		HDRRendering,
+		HDRExposure,
+		HDRStrength,
+		LightBloom,
+		BloomStrength,
+		BloomThreshold,
+		GlareStrength,
+		GlareLength,
+		Apply,
+		Cancel,
+		Count
+	};
+
 	void UpdateLightingSettingsMenu();
 }
