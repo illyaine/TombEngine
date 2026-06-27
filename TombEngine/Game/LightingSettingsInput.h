02228fe77d5f43335607bee0b7e9d615b18a66f9
Christoph Summerer <info@trlevel.de>
2026-06-27 23:55:47 +0200

    Open Lighting and HDR settings from Display settings

--- TombEngine/Game/LightingSettingsInput.h
+++ TombEngine/Game/LightingSettingsInput.h
@@ -1,8 +1,23 @@
 #pragma once
 
+#include "Game/Gui.h"
 #include "Game/LightingSettings.h"
+#include "Specific/Input/Input.h"
 
 namespace TEN::Gui
 {
-	bool UpdateLightingSettingsInput(bool& restartRequired);
+	inline bool UpdateLightingSettingsInput(bool& restartRequired)
+	{
+		if (g_Gui.GetMenuToDisplay() == Menu::Display &&
+			g_Gui.GetSelectedOption() == 7 &&
+			(IsClicked(In::Select) || IsClicked(In::Action)))
+		{
+			g_Gui.SetMenuToDisplay(Menu::LightingHDR);
+			g_Gui.SetSelectedOption(0);
+			restartRequired = false;
+			return true;
+		}
+
+		return g_Gui.GetMenuToDisplay() == Menu::LightingHDR;
+	}
 }
