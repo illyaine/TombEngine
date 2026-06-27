a1c551fb233bbe871489f294ab027c102da48cd2
Christoph Summerer <info@trlevel.de>
2026-06-28 00:03:05 +0200

    Render Lighting and HDR setting values

--- TombEngine/Game/LightingSettingsRender.h
+++ TombEngine/Game/LightingSettingsRender.h
@@ -28,5 +28,53 @@ namespace TEN::Gui
 			g_GameFlow->GetSettings()->UI.PlainTextColor, flags);
 	}
 
-	void RenderLightingSettings(TEN::Renderer::Renderer& renderer, bool restartRequired);
+	inline void RenderLightingSettings(TEN::Renderer::Renderer& renderer, bool restartRequired)
+	{
+		constexpr int center = 400;
+		constexpr int centerFlags = (int)PrintStringFlags::Outline | (int)PrintStringFlags::Center;
+		constexpr int spacing = 28;
+		int y = 75;
+
+		auto& settings = g_Gui.GetCurrentSettings().Configuration;
+		renderer.AddString(center, y, g_GameFlow->GetString(STRING_LIGHTING_HDR),
+			g_GameFlow->GetSettings()->UI.OptionTextColor, centerFlags);
+		y = 135;
+
+		AddLightingSettingRow(renderer, y, 0, STRING_HDR_RENDERING,
+			g_GameFlow->GetString(settings.EnableHDRRendering ? STRING_ENABLED : STRING_DISABLED));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 1, STRING_HDR_EXPOSURE, LightingPercent(settings.HDRExposure));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 2, STRING_HDR_STRENGTH, LightingPercent(settings.HDRStrength));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 3, STRING_LIGHT_BLOOM,
+			g_GameFlow->GetString(settings.EnableLightBloom ? STRING_ENABLED : STRING_DISABLED));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 4, STRING_BLOOM_STRENGTH, LightingPercent(settings.BloomStrength));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 5, STRING_BLOOM_THRESHOLD, LightingPercent(settings.BloomThreshold));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 6, STRING_GLARE_STRENGTH, LightingPercent(settings.GlareStrength));
+		y += spacing;
+		AddLightingSettingRow(renderer, y, 7, STRING_GLARE_LENGTH, LightingPercent(settings.GlareLength));
+		y += 42;
+
+		int applyFlags = centerFlags |
+			(g_Gui.GetSelectedOption() == 8 ? (int)PrintStringFlags::Blink : 0);
+		renderer.AddString(center, y, g_GameFlow->GetString(STRING_APPLY),
+			g_GameFlow->GetSettings()->UI.HeaderTextColor, applyFlags);
+		y += spacing;
+
+		int cancelFlags = centerFlags |
+			(g_Gui.GetSelectedOption() == 9 ? (int)PrintStringFlags::Blink : 0);
+		renderer.AddString(center, y, g_GameFlow->GetString(STRING_CANCEL),
+			g_GameFlow->GetSettings()->UI.HeaderTextColor, cancelFlags);
+
+		if (restartRequired)
+		{
+			y += 35;
+			renderer.AddString(center, y, g_GameFlow->GetString(STRING_HDR_RESTART_REQUIRED),
+				g_GameFlow->GetSettings()->UI.PlainTextColor, centerFlags);
+		}
+	}
 }
