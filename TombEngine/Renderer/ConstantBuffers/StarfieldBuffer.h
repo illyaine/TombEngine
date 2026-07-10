98270021d27aea164912f72838d5c41ac564d135
Christoph Summerer <info@trlevel.de>
2026-07-10 22:42:57 +0200

    Extend GPU environment constants

--- TombEngine/Renderer/ConstantBuffers/StarfieldBuffer.h
+++ TombEngine/Renderer/ConstantBuffers/StarfieldBuffer.h
@@ -5,8 +5,22 @@ namespace TEN::Renderer::ConstantBuffers
 {
 	using namespace DirectX::SimpleMath;
 
+	enum class GpuEnvironmentMode : int
+	{
+		Starfield = 0,
+		UnderwaterDust = 1,
+		Snow = 2,
+		Rain = 3
+	};
+
 	struct alignas(16) CStarfieldBuffer
 	{
 		Vector4 UV[2];
+		GpuEnvironmentMode Mode = GpuEnvironmentMode::Starfield;
+		int ClusterStride = 1;
+		float ClusterSpread = 0.0f;
+		float Padding = 0.0f;
 	};
+
+	static_assert(sizeof(CStarfieldBuffer) == 48);
 }
