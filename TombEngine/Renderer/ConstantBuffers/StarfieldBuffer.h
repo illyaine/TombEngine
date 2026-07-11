517cb9aad08688a388852ea3642340e8973e7633
Christoph Summerer <info@trlevel.de>
2026-07-11 16:17:47 +0200

    Keep legacy starfield buffer name as compatibility alias

--- TombEngine/Renderer/ConstantBuffers/StarfieldBuffer.h
+++ TombEngine/Renderer/ConstantBuffers/StarfieldBuffer.h
@@ -1,37 +1,9 @@
 #pragma once
-#include <SimpleMath.h>
+
+#include "Renderer/ConstantBuffers/GpuEnvironmentBuffer.h"
 
 namespace TEN::Renderer::ConstantBuffers
 {
-	using namespace DirectX::SimpleMath;
-
-	enum class GpuEnvironmentMode : int
-	{
-		Starfield = 0,
-		UnderwaterDust = 1,
-		Snow = 2,
-		Rain = 3
-	};
-
-	enum class GpuEnvironmentTextureMode : int
-	{
-		Bucket = 0,
-		Atlas = 1,
-		Array = 2
-	};
-
-	struct alignas(16) CStarfieldBuffer
-	{
-		Vector4 UV[2];
-		GpuEnvironmentMode Mode = GpuEnvironmentMode::Starfield;
-		int ClusterStride = 1;
-		float ClusterSpread = 0.0f;
-		int ParticleOffset = 0;
-		GpuEnvironmentTextureMode TextureMode = GpuEnvironmentTextureMode::Bucket;
-		int Padding0 = 0;
-		int Padding1 = 0;
-		int Padding2 = 0;
-	};
-
-	static_assert(sizeof(CStarfieldBuffer) == 64);
+	// Compatibility alias for existing renderer call sites.
+	using CStarfieldBuffer = CGpuEnvironmentBuffer;
 }
