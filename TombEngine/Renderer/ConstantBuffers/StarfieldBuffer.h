445104abe4c09f8720b54fdde0d309eeeb2b7dac
Christoph Summerer <info@trlevel.de>
2026-07-11 14:13:18 +0200

    Add GPU weather texture batching mode

--- TombEngine/Renderer/ConstantBuffers/StarfieldBuffer.h
+++ TombEngine/Renderer/ConstantBuffers/StarfieldBuffer.h
@@ -13,6 +13,13 @@ namespace TEN::Renderer::ConstantBuffers
 		Rain = 3
 	};
 
+	enum class GpuEnvironmentTextureMode : int
+	{
+		Bucket = 0,
+		Atlas = 1,
+		Array = 2
+	};
+
 	struct alignas(16) CStarfieldBuffer
 	{
 		Vector4 UV[2];
@@ -20,7 +27,11 @@ namespace TEN::Renderer::ConstantBuffers
 		int ClusterStride = 1;
 		float ClusterSpread = 0.0f;
 		int ParticleOffset = 0;
+		GpuEnvironmentTextureMode TextureMode = GpuEnvironmentTextureMode::Bucket;
+		int Padding0 = 0;
+		int Padding1 = 0;
+		int Padding2 = 0;
 	};
 
-	static_assert(sizeof(CStarfieldBuffer) == 48);
+	static_assert(sizeof(CStarfieldBuffer) == 64);
 }
