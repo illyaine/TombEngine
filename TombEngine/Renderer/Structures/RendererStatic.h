da519e5b9ade596f602372fbd334ec9c0e2ba95e
Christoph Summerer <info@trlevel.de>
2026-07-12 16:28:41 +0200

    Limit cached static light candidates

--- TombEngine/Renderer/Structures/RendererStatic.h
+++ TombEngine/Renderer/Structures/RendererStatic.h
@@ -6,6 +6,53 @@
 
 namespace TEN::Renderer::Structures
 {
+	class RendererStaticLightCache
+	{
+	private:
+		std::array<RendererLightNode, MAX_LIGHTS_PER_ITEM> _nodes = {};
+		size_t _size = 0;
+
+	public:
+		void clear()
+		{
+			_size = 0;
+		}
+
+		size_t capacity() const
+		{
+			return MAX_LIGHTS_PER_ITEM;
+		}
+
+		void reserve(size_t)
+		{
+			// Fixed-size cache; no allocation is required.
+		}
+
+		void push_back(const RendererLightNode& node)
+		{
+			if (_size < MAX_LIGHTS_PER_ITEM)
+			{
+				_nodes[_size++] = node;
+				return;
+			}
+
+			size_t weakestIndex = 0;
+			for (size_t i = 1; i < _size; i++)
+			{
+				if (_nodes[i].LocalIntensity < _nodes[weakestIndex].LocalIntensity)
+					weakestIndex = i;
+			}
+
+			if (node.LocalIntensity > _nodes[weakestIndex].LocalIntensity)
+				_nodes[weakestIndex] = node;
+		}
+
+		auto begin() { return _nodes.begin(); }
+		auto end() { return _nodes.begin() + _size; }
+		auto begin() const { return _nodes.begin(); }
+		auto end() const { return _nodes.begin() + _size; }
+	};
+
 	struct RendererStatic
 	{
 		int ObjectNumber;
@@ -18,8 +65,8 @@ namespace TEN::Renderer::Structures
 		Vector4 Color;
 		Vector4 AmbientLight;
 
-		std::vector<RendererLight*>	   LightsToDraw;
-		std::vector<RendererLightNode> CachedRoomLights;
+		std::vector<RendererLight*> LightsToDraw;
+		RendererStaticLightCache CachedRoomLights;
 		bool CacheLights;
 
 		BoundingSphere OriginalSphere;
