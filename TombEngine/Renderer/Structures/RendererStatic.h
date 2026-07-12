ee3660a5206c12647ee14acfab410b3753d36630
Christoph Summerer <info@trlevel.de>
2026-07-12 16:33:59 +0200

    Restore static light cache API compatibility

--- TombEngine/Renderer/Structures/RendererStatic.h
+++ TombEngine/Renderer/Structures/RendererStatic.h
@@ -1,63 +1,11 @@
 #pragma once
 
-#include <algorithm>
-#include <array>
-#include <cstddef>
-#include <vector>
-
 #include "Math/Objects/GameBoundingBox.h"
 #include "Math/Objects/Pose.h"
 #include "Renderer/Structures/RendererLight.h"
 
 namespace TEN::Renderer::Structures
 {
-	class RendererStaticLightCache
-	{
-	private:
-		std::array<RendererLightNode, MAX_LIGHTS_PER_ITEM> _nodes = {};
-		size_t _size = 0;
-
-	public:
-		void clear()
-		{
-			_size = 0;
-		}
-
-		size_t capacity() const
-		{
-			return MAX_LIGHTS_PER_ITEM;
-		}
-
-		void reserve(size_t)
-		{
-			// Fixed-size cache; no allocation is required.
-		}
-
-		void push_back(const RendererLightNode& node)
-		{
-			if (_size < MAX_LIGHTS_PER_ITEM)
-			{
-				_nodes[_size++] = node;
-				return;
-			}
-
-			size_t weakestIndex = 0;
-			for (size_t i = 1; i < _size; i++)
-			{
-				if (_nodes[i].LocalIntensity < _nodes[weakestIndex].LocalIntensity)
-					weakestIndex = i;
-			}
-
-			if (node.LocalIntensity > _nodes[weakestIndex].LocalIntensity)
-				_nodes[weakestIndex] = node;
-		}
-
-		auto begin() { return _nodes.begin(); }
-		auto end() { return _nodes.begin() + _size; }
-		auto begin() const { return _nodes.begin(); }
-		auto end() const { return _nodes.begin() + _size; }
-	};
-
 	struct RendererStatic
 	{
 		int ObjectNumber;
@@ -70,8 +18,8 @@ namespace TEN::Renderer::Structures
 		Vector4 Color;
 		Vector4 AmbientLight;
 
-		std::vector<RendererLight*> LightsToDraw;
-		RendererStaticLightCache CachedRoomLights;
+		std::vector<RendererLight*>	   LightsToDraw;
+		std::vector<RendererLightNode> CachedRoomLights;
 		bool CacheLights;
 
 		BoundingSphere OriginalSphere;
@@ -81,11 +29,11 @@ namespace TEN::Renderer::Structures
 		{
 			auto pos = Vector3::Lerp(PrevPose.Position.ToVector3(), Pose.Position.ToVector3(), interpolationFactor);
 			auto scale = Vector3::Lerp(PrevPose.Scale, Pose.Scale, interpolationFactor);
-
+			
 			auto translationMatrix = Matrix::CreateTranslation(pos);
 			auto scaleMatrix = Matrix::CreateScale(scale);
 			auto rotMatrix = Matrix::Lerp(PrevPose.Orientation.ToRotationMatrix(), Pose.Orientation.ToRotationMatrix(), interpolationFactor);
-
+			
 			auto worldMatrix = rotMatrix * scaleMatrix * translationMatrix;
 
 			auto sphereCenter = Vector3::Transform(OriginalSphere.Center, worldMatrix);
@@ -94,7 +42,7 @@ namespace TEN::Renderer::Structures
 
 			World = worldMatrix;
 			Sphere = BoundingSphere(sphereCenter, sphereRadius);
-
+			
 			CacheLights = true;
 		}
 	};
